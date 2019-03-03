d562fca3dd1567315a0566675d70231b9fc98ec8
Mariano Alberto Martinez Apolinar <IronDev@IronDevPro.local>
2019-03-03 14:56:55 -0600

    master - feature(app): Declaracion de variables con let vs var

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hola mundo con tsconfig.json");
+var a = 7;
+var b = 12;
+if (a === 7) {
+    var a_1 = 4;
+    var b = 1;
+    console.log('Dentro del if: ' + a_1 + ' - ' + b);
+}
+console.log('Fuera del if: ' + a + ' - ' + b);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("hola mundo con tsconfig.json");
+var a = 7;
+var b = 12;
+
+if (a === 7) {
+    let a = 4;
+    var b = 1;
+
+    console.log('Dentro del if: ' + a + ' - ' + b );
+
+}
+
+console.log('Fuera del if: ' + a + ' - ' + b);
