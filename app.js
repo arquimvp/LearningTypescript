aa2d7295b5f643e39edcba1a7d0aab5ab0e4426b
IronDev <alberto.martinez@kairosds.com>
2019-03-03 21:23:21 -0600

    master - feature(app): Uso de enum

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var superHeroe = ["hulk", 12, true];
-superHeroe.push('hola');
-console.log(superHeroe);
+//ejemplo de un control de volume en una app de audio.
+//Me sirve para hacer referencia a algun valor a traves de una descripcion mayormente logica.
+var Volume;
+(function (Volume) {
+    Volume[Volume["min"] = 1] = "min";
+    Volume[Volume["medio"] = 5] = "medio";
+    Volume[Volume["max"] = 10] = "max";
+})(Volume || (Volume = {}));
+var currentVolume = Volume.medio;
+console.log(currentVolume);
+console.log(Volume);
--- app.ts
+++ app.ts
@@ -1,5 +1,13 @@
-let superHeroe: [string, number, boolean] = ["hulk",12,true];
+//ejemplo de un control de volume en una app de audio.
+//Me sirve para hacer referencia a algun valor a traves de una descripcion mayormente logica.
 
-superHeroe.push('hola');
+enum Volume{
+    min = 1,
+    medio = 5,
+    max = 10
+}
 
-console.log(superHeroe);
+let currentVolume: number = Volume.medio;
+
+console.log(currentVolume);
+console.log(Volume);
