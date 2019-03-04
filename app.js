52027b67af7fdddfc4abae013ebfde1597bd5389
IronDev <alberto.martinez@kairosds.com>
2019-03-04 00:01:09 -0600

    master - feature(app): Uso y diferencias de null y undefined

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-var cualquierCosa = "cualquier valor";
-var longitud = cualquierCosa.length;
-console.log(longitud);
-//En este caso yo se con toda seguridad que el valor a castear es un string, por lo cual
-//puedo obtener su longitud.
+//undefined:
+//Cuando una variable declarada no es definida en tiempo de ejecucion se considera "undefined".
+//Tamben aplica a los parametros de una funcion.
+var myUndefined;
+console.log(myUndefined);
+console.log(typeof (myUndefined));
+function foo(myvar) {
+    console.log(myvar);
+}
+foo(1);
+//null:
+//representa la ausencia de valor intencional, nosotros como devs debemos usar null cuando no sabremos
+//que valor contendra dicah variable.
+var myNull = null;
+console.log(myNull);
+console.log(typeof (myNull));
+//en esta ultima se muestra object, es un bug pero asi es javascript.
+//Dentro de funciones una buena practica es validar si el parametro es null o undefined:
+function validador(param) {
+    if (param) {
+        console.log('param no es null ni undefined');
+    }
+    else {
+        console.log(param);
+    }
+}
+var algo;
+validador(algo);
--- app.ts
+++ app.ts
@@ -1,7 +1,34 @@
-let cualquierCosa : any = "cualquier valor";
+//undefined:
+//Cuando una variable declarada no es definida en tiempo de ejecucion se considera "undefined".
+//Tamben aplica a los parametros de una funcion.
 
-let longitud : number = (<string>cualquierCosa).length;
+let myUndefined;
+console.log(myUndefined);
+console.log(typeof(myUndefined));
 
-console.log(longitud);
-//En este caso yo se con toda seguridad que el valor a castear es un string, por lo cual
-//puedo obtener su longitud.
+function foo(myvar:any) {
+    console.log(myvar);
+}
+
+foo(1);
+
+//null:
+//representa la ausencia de valor intencional, nosotros como devs debemos usar null cuando no sabremos
+//que valor contendra dicah variable.
+
+let myNull = null;
+console.log(myNull);
+console.log(typeof(myNull));
+
+//en esta ultima se muestra object, es un bug pero asi es javascript.
+
+//Dentro de funciones una buena practica es validar si el parametro es null o undefined:
+function validador(param:any) {
+    if(param){
+        console.log('param no es null ni undefined');
+    }else{
+        console.log(param);
+    }
+}
+let algo;
+validador(algo);
