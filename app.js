f4c88fef55552eb62bb6273cd6233c7fef5b309f
IronDev <alberto.martinez@kairosds.com>
2019-03-04 00:31:41 -0600

    master - challenge(app): solucion en typescript

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ var villano = ["Lex Lutor", 5, true];
 // Arreglos
 var aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
 //Enumeraciones
-var fuerzaFlash = 5;
-var fuerzaSuperman = 100;
-var fuerzaBatman = 1;
-var fuerzaAcuaman = 0;
+var Fuerza;
+(function (Fuerza) {
+    Fuerza[Fuerza["acuaman"] = 0] = "acuaman";
+    Fuerza[Fuerza["batman"] = 1] = "batman";
+    Fuerza[Fuerza["flash"] = 5] = "flash";
+    Fuerza[Fuerza["superman"] = 100] = "superman";
+})(Fuerza || (Fuerza = {}));
+;
+var fuerzaFlash = Fuerza.flash;
+var fuerzaSuperman = Fuerza.superman;
+var fuerzaBatman = Fuerza.batman;
+var fuerzaAcuaman = Fuerza.acuaman;
 // Retorno de funciones
-function activar_batiseñal() {
+function actilet_batiseñal() {
     return "activada";
 }
 function pedir_ayuda() {
