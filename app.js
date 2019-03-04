606b08f628185cb6eb49ad9b9d48bbd6c75377a1
IronDev <alberto.martinez@kairosds.com>
2019-03-04 00:19:16 -0600

    master - challenge(app): Codigo JS, redefinir a typescript

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-//undefined:
-//Cuando una variable declarada no es definida en tiempo de ejecucion se considera "undefined".
-//Tamben aplica a los parametros de una funcion.
-var myUndefined;
-console.log(myUndefined);
-console.log(typeof (myUndefined));
-function foo(myvar) {
-    console.log(myvar);
+// Tipos
+var batman = "Bruce";
+var superman = "Clark";
+var existe = false;
+// Tuplas
+var parejaHeroes = [batman, superman];
+var villano = ["Lex Lutor", 5, true];
+// Arreglos
+var aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+//Enumeraciones
+var fuerzaFlash = 5;
+var fuerzaSuperman = 100;
+var fuerzaBatman = 1;
+var fuerzaAcuaman = 0;
+// Retorno de funciones
+function activar_batiseñal() {
+    return "activada";
 }
-foo(1);
-//null:
-//representa la ausencia de valor intencional, nosotros como devs debemos usar null cuando no sabremos
-//que valor contendra dicah variable.
-var myNull = null;
-console.log(myNull);
-console.log(typeof (myNull));
-//en esta ultima se muestra object, es un bug pero asi es javascript.
-//Dentro de funciones una buena practica es validar si el parametro es null o undefined:
-function validador(param) {
-    if (param) {
-        console.log('param no es null ni undefined');
-    }
-    else {
-        console.log(param);
-    }
+function pedir_ayuda() {
+    console.log("Auxilio!!!");
 }
-var algo;
-validador(algo);
+// Aserciones de Tipo
+var poder = "100";
+var largoDelPoder = poder.length;
+console.log(largoDelPoder);
